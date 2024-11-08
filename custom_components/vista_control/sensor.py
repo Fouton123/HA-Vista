f26f9c4310de0a34c1e562a75dcf053a3d1530a8
Eric <erickausch@comcast.net>
2024-11-07 19:03:55 -0500

    aread

--- custom_components/vista_control/sensor.py
+++ custom_components/vista_control/sensor.py
@@ -271,6 +271,7 @@ class SerialSensor(SensorEntity):
 
     async def async_added_to_hass(self):
         """Handle when an entity is about to be added to Home Assistant."""
+        _LOGGER.error("start aRead")
         self._serial_loop_task = self.hass.loop.create_task(
             self.serial_read()
         )
