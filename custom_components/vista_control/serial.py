d86c031d4a8e29e3c88ab76cc0df5da9a7f6fa2b
Eric <erickausch@comcast.net>
2024-11-13 17:11:19 -0500

    read

--- custom_components/vista_control/serial.py
+++ custom_components/vista_control/serial.py
@@ -75,17 +75,9 @@ class SerialComm():
         if self.udp is None:
             await self.serial_open()
 
-        while True:
-            if self._interupt == False:
-                try:
-                    line = self.udp.recv(1024)
-                    _LOGGER.info(line)
-                except Exception as e:
-                    _LOGGER.info("error", e)
-                else:
-                    self.line = line[0].decode("utf-8").strip()
-
-                    return self.line
+        line = self.udp.recv(1024)
+        self.line = line
+        return self.line
 
     async def serial_open(self):
         logged_error = False
